2cebc080b87c540b9613e8f220d512e9e6fe782e
Taro <zettaigoukaku547@gmail.com>
2020-01-06 17:14:11 +0900

    [add]モデルスペック作成

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :task do
-    
+    title { 'たちつてと' }
+    status { 1 }
   end
 end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'バリデーション' do
+    let(:task_a){FactoryBot.build(:task)}
+    let(:task_b){FactoryBot.build(:task, title: nil)}
+    let(:task_c){FactoryBot.build(:task, status: nil)}
+    let(:task_d){FactoryBot.build(:task, title: nil, status: nil)}
+
+    context 'title,statusを入力している時' do
+      it 'is valid with title, status' do
+        expect(task_a).to be_valid
+      end
+    end
+
+    context 'titleが入力されていない時' do
+      it 'is invalid without a title' do
+        task_b.valid?
+        expect(task_b.valid?).to eq(false)
+        #expect(task.errors[:title]).to include("を入力してください")
+      end
+    end
+
+    context 'statusが入力されていない時' do
+      it 'is invalid without a status' do
+        task_c.valid?
+        expect(task_c.valid?).to eq(false)
+        #expect(task.errors[:status]).to include("を入力してください")
+      end
+    end
+
+    context 'title,statusが入力されていない時' do
+      it 'is invalid without title,status' do
+        task_d.valid?
+        expect(task_d.valid?).to eq(false)
+        #expect(task.errors[:title]).to include("を入力してください")
+        #expect(task.errors[:status]).to include("を入力してください")
+      end
+    end
+  end
 end
