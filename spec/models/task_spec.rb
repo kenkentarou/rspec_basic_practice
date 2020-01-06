f653fee8c5d03be742b22a6ffaa7d926679615f0
Taro <zettaigoukaku547@gmail.com>
2020-01-06 20:59:17 +0900

    [add]テストコード修正

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -2,39 +2,45 @@ require 'rails_helper'
 
 RSpec.describe Task, type: :model do
   describe 'バリデーション' do
-    let(:task_a){FactoryBot.build(:task)}
-    let(:task_b){FactoryBot.build(:task, title: nil)}
-    let(:task_c){FactoryBot.build(:task, status: nil)}
-    let(:task_d){FactoryBot.build(:task, title: nil, status: nil)}
+    let(:task){build(:task)}
+    let(:task_without_title){build(:task, title: nil)}
+    let(:task_without_status){build(:task, status: nil)}
+    let(:task_withou_title_status){build(:task, title: nil, status: nil)}
+    #let(:task_d){create(:task)}
 
     context 'title,statusを入力している時' do
       it 'is valid with title, status' do
-        expect(task_a).to be_valid
+        expect(task).to be_valid
       end
     end
 
     context 'titleが入力されていない時' do
       it 'is invalid without a title' do
-        task_b.valid?
-        expect(task_b.valid?).to eq(false)
-        #expect(task.errors[:title]).to include("を入力してください")
+        task_without_title.valid?
+        expect(task_without_title.valid?).to eq(false)
       end
     end
 
     context 'statusが入力されていない時' do
       it 'is invalid without a status' do
-        task_c.valid?
-        expect(task_c.valid?).to eq(false)
-        #expect(task.errors[:status]).to include("を入力してください")
+        task_without_status.valid?
+        expect(task_without_status.valid?).to eq(false)
       end
     end
 
     context 'title,statusが入力されていない時' do
       it 'is invalid without title,status' do
-        task_d.valid?
-        expect(task_d.valid?).to eq(false)
-        #expect(task.errors[:title]).to include("を入力してください")
-        #expect(task.errors[:status]).to include("を入力してください")
+        task_withou_title_status.valid?
+        expect(task_withou_title_status.valid?).to eq(false)
+      end
+    end
+
+    context 'titleが一意でない時' do
+      it 'is invalid with a duplicate title' do
+        FactoryBot.create(:task, title: 'やゆよ')
+        task_unique = build(:task, title: 'やゆよ')
+        task_unique.valid?
+        expect(task_unique.valid?).to eq(false)
       end
     end
   end
